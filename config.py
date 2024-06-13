6bac312c3716f184878a5175ff14e9e34d5aa6a6
Elisa Mariotti <allievo_Mariotti.E@itsumbria.it>
2024-06-13 15:38:07 +0200

    Aggiunte variabili costanti

--- config.py
+++ config.py
@@ -4,3 +4,8 @@ che devono essere ripetute negli altri moduli)
 
 N.B. per convenzione scriviamole COSI_PER_FAVORE"""
 # STILE_BOTTONE_FLAGGATO = ...
+
+button_width = 10
+button_height = 2
+image_path = "icona.png"
+
