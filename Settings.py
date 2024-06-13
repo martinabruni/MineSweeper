7c67d0dd2e3b7de746dbcb1c25d979b4c54411c6
Elisa Mariotti <allievo_Mariotti.E@itsumbria.it>
2024-06-13 16:56:39 +0200

    creata classe settings da modificare ancora

---
+++ Settings.py
@@ -0,0 +1,65 @@
+import tkinter as tk
+import menuLib as mL
+import tkinter.font as tkFont
+from tkinter import PhotoImage
+import cv2
+# import Settings
+from PIL import Image, ImageTk
+from config import*
+
+
+class Settings:
+    
+    def __init__(self,root,frame):
+        self.__root=root
+        self.__frame=frame
+        
+    @property
+    def root(self):
+     return self.__root
+   
+    @property
+    def frame(self):
+      return self.__frame
+     
+    
+    def run_model(self,root):
+       
+        def set_image():
+            image = cv2.imread(image_settings)
+            image = cv2.resize(image, (150, 150))  
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            photo = ImageTk.PhotoImage(image=Image.fromarray(image))
+
+            return photo
+    
+        mL.set_Frame(root,frame)
+    
+        def fill_Frame(self):
+            
+            image = set_image()
+         #IMMMAGINE 
+            image_label = tk.Label(self.__frame, image=image, bg="black")
+            image_label.place(relx=0.5, rely=0.2, anchor='center')
+
+        #TITOLO
+    
+            custom_font = tkFont.Font(family="Terminal", size=25)
+
+            label = tk.Label(self.__frame, text=" SETTINGS",font=custom_font,fg="white", bg="black")
+            label.place(relx=0.5, rely=0.4, anchor='center')
+
+        #BUTTON
+
+        
+            buttonSave =mL.create_button(self.__frame,"Save", open_second_module(self.__frame, buttonSave))
+            buttonSave.place(relx=0.5, rely=0.7, anchor='center')
+
+        
+        
+        
+    
+
+
+
+
