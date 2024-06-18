cbf492109228bf4062ed4e165ab03e024d028b5e
martinabruni <mbruni02@gmail.com>
2024-06-18 19:23:31 +0200

    Game title fixed :)

--- Utils/generic.py
+++ Utils/generic.py
@@ -28,7 +28,7 @@ escapeRoot = None
 rootGlobal = tk.Tk()
 frameGlobal = tk.Frame(rootGlobal)
 gameController = GameController()
-menuGlobal = Menu(75, "Merda", imagePath)
+menuGlobal = Menu(75, "Campo Minato", imagePath)
 # settingsGlobal = Settings(rootGlobal, frameGlobal)
 
 # Core gameplay objects
